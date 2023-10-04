98030b218bb98f782ce98f106ad79b6cc64db6c9
stephan hantigk <s.hantigk@runlevel3.de>
2023-10-04 15:55:13 +0200

    Another fix

--- createVenv.sh
+++ createVenv.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
-venv_dir="`dirname "$0"`/.venv"
+this_dir="`dirname "$0"`"
+venv_dir="$this_dir/.venv"
 
 if [ -d "$venv_dir" ]; then
     echo "Venv directory '$venv_dir' seems to exist. Exiting."
@@ -11,5 +12,5 @@ python3 -m venv "$venv_dir"
 
 source "$venv_dir/bin/activate"
 
-pip3 install -r "$venv_dir/requirements.txt"
+pip3 install -r "$this_dir/requirements.txt"
 
